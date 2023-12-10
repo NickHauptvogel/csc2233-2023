97b25d4f9226b278e41befe9d4269a58a3e9175c
Nick Hauptvogel <nick@hauptvogel.cc>
2023-12-10 09:23:31 -0500

    Added tuned_2 results

--- debug.py
+++ debug.py
@@ -28,7 +28,6 @@ def plot_histogram(test_score, test_label, name):
             alpha=0.5, color='b')
     ax.set_xlabel('Anomaly score')
     ax.set_ylabel('Density')
-    ax.set_yscale('log')
     ax.legend(['Anomalies', 'Normal'])
     ax.set_title('Histogram of anomaly scores for ' + name)
     plt.show()
@@ -58,47 +57,60 @@ def generate_bf_search_pkl(result_dir, test_score, y_test, min):
     pickle.dump(arr, open(os.path.join(result_dir, 'bf_search.pkl'), 'wb'))
 
 
+def get_bf_search_score(result_dir, test_data_path, name, thr=None, days=14):
+    test_score = pickle.load(open(os.path.join(result_dir, 'test_score.pkl'), 'rb'))
+    test_data = pickle.load(open(test_data_path, 'rb'))
+    config = json.load(open(os.path.join(result_dir, 'config.defaults.json'), 'r'))
+    window_length = config['window_length']
+    # Convert test data to DataFrame
+    test_df = pd.DataFrame(test_data)
+    # Group test_data by first column (serial number) and cut off first window_length rows
+    test_df = test_df.groupby(test_df.columns[0], sort=False).apply(lambda x: x.iloc[window_length - 1:, :])
+    # Reset index
+    test_df = test_df.reset_index(drop=True)
+    # Group by serial number, if last row is anomaly, label as anomaly
+    anomaly_count = test_df.groupby(test_df.columns[0], sort=False).apply(lambda x: x.iloc[-1, -1]).value_counts()
+    print(name + ": Anomaly count: ", anomaly_count)
+    def change_label(x):
+        if x.iloc[-1, -1] == 1:
+            x.iloc[:, -1] = 0
+            x.iloc[:-days, -1] = 1
+        return x
+    test_df = test_df.groupby(test_df.columns[0], sort=False).apply(change_label)
+    # Concatenate test_score
+    test_df = pd.concat([test_df, pd.DataFrame(test_score)], axis=1)
+    # Use last row as label
+    test_label = test_df.iloc[:, -2].values
+
+    # If bf_search.pkl does not exist, generate it
+    if not os.path.exists(os.path.join(result_dir, 'bf_search.pkl')):
+        generate_bf_search_pkl(result_dir, test_score, test_label, -100000)
+
+    bf_search_score = pickle.load(open(os.path.join(result_dir, 'bf_search.pkl'), 'rb'))
+
+    # Get FDR for FAR = 0.01
+    fdr = bf_search_score[:, 1][np.where(bf_search_score[:, 2] > 0.01)][0]
+    thr_001 = bf_search_score[:, -1][np.where(bf_search_score[:, 2] > 0.01)][0]
+    print(name + ": FDR for FAR = 0.01: ", fdr, " with threshold: ", thr_001)
+    if thr is not None:
+        fdr = bf_search_score[:, 1][np.where(bf_search_score[:, -1] > thr)][0]
+        print(name + ": FDR for FAR = 0.01: ", fdr, " with threshold: ", thr)
+
+    plot_histogram(test_score, test_label, name)
+
+    return bf_search_score, thr_001
+
+
 if __name__ == '__main__':
 
-
-    def get_bf_search_score(result_dir, test_data_path, name, thr=None):
-        test_score = pickle.load(open(os.path.join(result_dir, 'test_score.pkl'), 'rb'))
-        test_data = pickle.load(open(test_data_path, 'rb'))
-        config = json.load(open(os.path.join(result_dir, 'config.defaults.json'), 'r'))
-        window_length = config['window_length']
-        # Convert test data to DataFrame
-        test_df = pd.DataFrame(test_data)
-        # Group test_data by first column (serial number) and cut off first window_length rows
-        test_df = test_df.groupby(test_df.columns[0], sort=False).apply(lambda x: x.iloc[window_length - 1:, :])
-        # Reset index
-        test_df = test_df.reset_index(drop=True)
-        # Use last row as label
-        test_label = test_df.iloc[:, -1].values
-
-        # If bf_search.pkl does not exist, generate it
-        if not os.path.exists(os.path.join(result_dir, 'bf_search.pkl')):
-            generate_bf_search_pkl(result_dir, test_score, test_label, -100000)
-
-        bf_search_score = pickle.load(open(os.path.join(result_dir, 'bf_search.pkl'), 'rb'))
-
-        # Get FDR for FAR = 0.01
-        fdr = bf_search_score[:, 1][np.where(bf_search_score[:, 2] > 0.01)][0]
-        thr_001 = bf_search_score[:, -1][np.where(bf_search_score[:, 2] > 0.01)][0]
-        print(name + ": FDR for FAR = 0.01: ", fdr, " with threshold: ", thr_001)
-        if thr is not None:
-            fdr = bf_search_score[:, 1][np.where(bf_search_score[:, -1] > thr)][0]
-            print(name + ": FDR for threshold: ", fdr, " with threshold: ", thr)
-
-        plot_histogram(test_score, test_label, name)
-
-        return bf_search_score, thr_001
-
     st8000, thr1 = get_bf_search_score('results/tuned_model_st8000/results', 'processed_st8000/test.pkl', 'st8000')
+    st8000_2, _ = get_bf_search_score('results/tuned_model_st8000_2/results', 'processed_st8000/test.pkl', 'st8000')
     st8000_exp1, thr2 = get_bf_search_score('results/tuned_model_st8000_exp1/results', 'processed_st12000/test.pkl', 'st8000_exp1', thr1)
 
     fig, ax = plt.subplots(1, 1)
 
     ax.plot(st8000[:, 2], st8000[:, 1], label='st8000 pre-train')
+    ax.plot(st8000_2[:, 2], st8000_2[:, 1], label='st8000 pre-train 2')
     ax.plot(st8000_exp1[:, 2], st8000_exp1[:, 1], label='st8000 TFL st12000 no train')
     # Vertical line at 0.01
     ax.axvline(x=0.01, color='r', linestyle='--')
@@ -108,7 +120,7 @@ if __name__ == '__main__':
     # Set y label
     ax.set_ylabel('FDR')
     # Set title
-    ax.set_title('FDR-FAR Tradeoff for pre-trained and transferred model')
+    ax.set_title('FDR-FAR Tradeoff')
     # x lim
-    ax.set_xlim([0, 0.1])
+    ax.set_xlim([0, 0.3])
     plt.show()
